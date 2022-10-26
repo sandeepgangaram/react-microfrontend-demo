85a470bea9747bb62c5bbdeac31d986c03e9379a
Sandeep Gangaram <sandeepgangaram@alumni.iitm.ac.in>
2022-10-26 22:20:11 +0530

    setup marketing webpack config for prod

--- marketing/config/webpack.dev.js
+++ marketing/config/webpack.dev.js
@@ -1,5 +1,4 @@
 const { merge } = require("webpack-merge");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const commonConfig = require("./webpack.common");
 const packageJson = require("../package.json");
@@ -20,9 +19,6 @@ const devConfig = {
         "./MarketingApp": "./src/bootstrap.js",
       },
       shared: packageJson.dependencies,
-    }),
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
     }),
   ],
 };
