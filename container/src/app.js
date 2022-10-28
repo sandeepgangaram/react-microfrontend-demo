8a250268c59ff274d5b2f1fa89f28aa89dc0e21f
Sandeep Gangaram <sandeepgangaram@alumni.iitm.ac.in>
2022-10-28 13:43:38 +0530

    add Header component in container

--- container/src/app.js
+++ container/src/app.js
@@ -1,13 +1,17 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import MarketingApp from "./components/MarketingApp";
+import Header from "./components/Header";
 
 const App = () => {
   return (
-    <div>
-      <h1>A Micro Frontend Architecture!</h1>
-      <hr />
-      <MarketingApp />
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header />
+        <hr />
+        <MarketingApp />
+      </div>
+    </BrowserRouter>
   );
 };
 
