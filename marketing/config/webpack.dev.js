a1e10efcf89422a1a12ec6c79ba771a2a913ec8d
Sandeep Gangaram <sandeepgangaram@alumni.iitm.ac.in>
2022-10-28 13:43:25 +0530

    add htmlwebpackplugin require statement

--- marketing/config/webpack.dev.js
+++ marketing/config/webpack.dev.js
@@ -1,5 +1,6 @@
 const { merge } = require("webpack-merge");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const commonConfig = require("./webpack.common");
 const packageJson = require("../package.json");
 
