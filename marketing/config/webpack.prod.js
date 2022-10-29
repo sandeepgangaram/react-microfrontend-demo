de0413284a7ae8146292d48f0c7baf40e7fa313b
Sandeep Gangaram <sandeepgangaram@alumni.iitm.ac.in>
2022-10-29 15:33:25 +0530

    fix webpack.config bug for marketing

--- marketing/config/webpack.prod.js
+++ marketing/config/webpack.prod.js
@@ -4,7 +4,7 @@ const commonConfig = require("./webpack.common");
 const packageJson = require("../package.json");
 
 const prodConfig = {
-  mode: "development",
+  mode: "production",
   output: {
     filename: "[name].[contenthash].js",
     publicPath: "/marketing/latest/",
