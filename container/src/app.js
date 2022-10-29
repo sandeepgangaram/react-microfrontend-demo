431001459d215cf7864d991bc252bc868c79dd34
Sandeep Gangaram <sandeepgangaram@alumni.iitm.ac.in>
2022-10-29 14:57:45 +0530

    classname prefix for production -container

--- container/src/app.js
+++ container/src/app.js
@@ -1,16 +1,23 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import { StylesProvider, createGenerateClassName } from "@material-ui/styles";
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
+
 const App = () => {
   return (
-    <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
-    </BrowserRouter>
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
+        <div className="test">
+          <Header />
+          <MarketingApp />
+        </div>
+      </BrowserRouter>
+    </StylesProvider>
   );
 };
 
