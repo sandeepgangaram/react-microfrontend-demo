7471423120a80e3faa74067376dd0ad3e53224ee
Sandeep Gangaram <sandeepgangaram@alumni.iitm.ac.in>
2022-10-29 14:55:57 +0530

    class name prefix for production - marketing

--- marketing/src/app.js
+++ marketing/src/app.js
@@ -1,14 +1,18 @@
 import React from "react";
 import { Switch, Route, BrowserRouter } from "react-router-dom";
-import { StylesProvider } from "@material-ui/core";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core";
 
 import Landing from "./components/Landing";
 import Pricing from "./components/Pricing";
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: "ma",
+});
+
 const App = () => {
   return (
     <div>
-      <StylesProvider>
+      <StylesProvider generateClassName={generateClassName}>
         <BrowserRouter>
           <Switch>
             <Route exact path="/pricing" component={Pricing} />
