433ed6809da86ecbd798c585f8765915efa202b4
Sandeep Gangaram <sandeepgangaram@alumni.iitm.ac.in>
2022-10-29 15:08:30 +0530

    fix import in marketing app.js

--- marketing/src/app.js
+++ marketing/src/app.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { Switch, Route, BrowserRouter } from "react-router-dom";
-import { StylesProvider, createGenerateClassName } from "@material-ui/core";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
 
 import Landing from "./components/Landing";
 import Pricing from "./components/Pricing";
