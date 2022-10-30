f646b644ea66ee315a1c931919d492a156a6a17c
Sandeep Gangaram <sandeepgangaram@alumni.iitm.ac.in>
2022-10-30 20:29:21 +0530

    enable marketing to container history sync

--- container/src/components/MarketingApp.js
+++ container/src/components/MarketingApp.js
@@ -1,11 +1,20 @@
 import { mount } from "marketing/MarketingApp";
 import React, { useRef, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 
 const MarketingApp = () => {
   const ref = useRef(null);
+  const history = useHistory();
 
   useEffect(() => {
-    mount(ref.current);
+    mount(ref.current, {
+      onNavigate: ({ pathname: nextPathname }) => {
+        const { pathname } = history.location;
+        if (pathname !== nextPathname) {
+          history.push(nextPathname);
+        }
+      },
+    });
   });
 
   return <div ref={ref} />;
--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -4,8 +4,10 @@ import { createMemoryHistory } from "history";
 import App from "./app";
 
 // Mount function to start up the app
-const mount = (el) => {
+const mount = (el, { onNavigate }) => {
   const history = createMemoryHistory();
+
+  history.listen(onNavigate);
 
   ReactDOM.render(<App history={history} />, el);
 };
