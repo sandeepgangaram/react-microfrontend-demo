364e1cb687a15b656060e3f89b3ec262ac5c1949
Sandeep Gangaram <sandeepgangaram@alumni.iitm.ac.in>
2022-10-31 22:06:12 +0530

    add dashboard config files

---
+++ dashboard/config/webpack.dev.js
@@ -0,0 +1,35 @@
+const { merge } = require("webpack-merge");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const commonConfig = require("./webpack.common");
+const packageJson = require("../package.json");
+
+const devConfig = {
+  mode: "development",
+  output: {
+    publicPath: "http://localhost:8083/",
+  },
+  devServer: {
+    port: 8083,
+    historyApiFallback: true,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "dashboard",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./DashboardApp": "./src/bootstrap.js",
+      },
+      shared: packageJson.dependencies,
+    }),
+
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
+
+module.exports = merge(commonConfig, devConfig);
