1cf9a671d3000f3138e81841ae09ebf30de34e1a
Sandeep Gangaram <sandeepgangaram@alumni.iitm.ac.in>
2022-10-31 11:45:34 +0530

    add initial memory path for marketing

--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -4,8 +4,12 @@ import { createMemoryHistory, createBrowserHistory } from "history";
 import App from "./app";
 
 // Mount function to start up the app
-const mount = (el, { onNavigate, defaultHistory }) => {
-  const history = defaultHistory || createMemoryHistory();
+const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
+  const history =
+    defaultHistory ||
+    createMemoryHistory({
+      initialEntries: [initialPath],
+    });
 
   if (onNavigate) {
     history.listen(onNavigate);
